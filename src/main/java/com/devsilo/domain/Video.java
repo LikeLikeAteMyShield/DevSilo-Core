ae84ee601c1471f89cba072bce196836d40f8ec1
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2017-02-16 20:31:36 +0000

    add image data to video response

--- src/main/java/com/devsilo/domain/Video.java
+++ src/main/java/com/devsilo/domain/Video.java
@@ -1,11 +1,19 @@
 package com.devsilo.domain;
 
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Video {
 
     private Id id;
     private String title;
     private String videoFilePath;
     private String thumbnailFilePath;
+    private String thumbnailData;
 
     public Video(Id id, String title, String videoFilePath, String thumbnailFilePath) {
         this.id = id;
@@ -28,5 +36,27 @@ public class Video {
 
     public String theThumbnailFilePath() {
         return thumbnailFilePath;
+    }
+
+    public String getThumbnailData() {
+
+        File image = new File(thumbnailFilePath);
+        byte[] imageBytes = new byte[(int)image.length()];
+
+        try {
+            FileInputStream inputStream = new FileInputStream(image);
+            inputStream.read(imageBytes);
+        } catch(FileNotFoundException e) {
+            System.out.println("File Not Found");
+            e.printStackTrace();
+        } catch(IOException e) {
+            System.out.println("Error Reading File");
+            e.printStackTrace();
+        }
+
+        byte[] imageBytesBase64 = Base64.encodeBase64(imageBytes);
+        String imageDataAsBase64 = new String(imageBytesBase64);
+
+        return imageDataAsBase64;
     }
 }
